38668991b60625906101d192dc11d37a68379e64
Cenaure <discordjahysama@gmail.com>
2026-07-28 23:58:49 +0200

    feat(user): add activation link generation after creating an account

--- nest-server/src/modules/users/users.service.ts
+++ nest-server/src/modules/users/users.service.ts
@@ -1,14 +1,19 @@
-import {Injectable} from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
 import {DatabaseService} from "../../database/database.service";
 import CreateUserDto from "./dto/create-user.dto";
 import {AppException} from "../../common/errors/app-exception";
 import {AppExceptionBodyCode} from "../../common/errors/app-exception-body.interface";
 import * as bcrypt from 'bcrypt';
+import {Cache, CACHE_MANAGER} from "@nestjs/cache-manager";
+import {randomUUID} from "crypto";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class UsersService {
   constructor(
-    private readonly databaseService: DatabaseService
+    private readonly databaseService: DatabaseService,
+    private readonly configService: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
   ) {
   }
 
@@ -30,9 +35,7 @@ export class UsersService {
 
     const passwordHash = await bcrypt.hash(dto.password, 12);
 
-    //TODO: activation link
-
-    return this.databaseService.user.create({
+    const user = await this.databaseService.user.create({
       data: {
         email: dto.email,
         username: dto.username,
@@ -40,6 +43,13 @@ export class UsersService {
         is_activated: false
       }
     })
+
+    const activationLinkTTL = this.configService.get("auth.activation_link_ttl")
+
+    const activation_id = randomUUID()
+    await this.cacheManager.set(`activation_id:${activation_id}`, user.id, activationLinkTTL);
+
+    return user;
   }
 
   async findByEmail(email: string) {
@@ -51,5 +61,12 @@ export class UsersService {
       where: {id: userId},
       data: {last_login_at: new Date()},
     });
+  }
+
+  async setIsActivated(userId: number) {
+    await this.databaseService.user.update({
+      where: {id: userId},
+      data: {is_activated: true}
+    })
   }
 }
