0b88d2030d66017e46d27d08b9a200f01a847bc1
Cenaure <discordjahysama@gmail.com>
2026-07-28 22:27:48 +0200

    feat(config): add node env config

--- nest-server/src/configurations/app.config.ts
+++ nest-server/src/configurations/app.config.ts
@@ -6,10 +6,12 @@ import validateConfig from "./_config.validator";
 export default registerAs("app", () => {
   return validateConfig(
     {
+      node_env: process.env.NODE_ENV || "development",
       server_port: parseInt(process.env.SERVER_PORT!, 10) || 3000,
     },
 
     Joi.object({
+      node_env: Joi.string().valid("development", "production", "test").required(),
       server_port: Joi.number().required(),
     })
   )
