925ecd8c1dea3b77c7fd4dc5e8b6feabe4e72b39
Cenaure <discordjahysama@gmail.com>
2026-07-28 22:23:00 +0200

    feat(cookies): add cookies service to standardize tokens' cookie handling

---
+++ nest-server/src/modules/auth/tokens-cookies.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@nestjs/common';
+import {Response} from "express";
+import {ConfigService} from "@nestjs/config";
+
+@Injectable()
+export class TokensCookiesService {
+  constructor(
+    private readonly configService: ConfigService
+  ) {
+  }
+
+  /**
+   * Add access and refresh tokens in the set-cookie header
+   */
+  setCookies(
+    response: Response,
+    tokens: { accessToken: string; refreshToken: string }
+  ) {
+    const nodeEnv = this.configService.get('app.node_env');
+    const domain = this.configService.get('frontend.domain');
+    const accessTokenTTL = this.configService.get('auth.access_token_ttl'); // 30 * 60 * 1000
+    const refreshTokenTTL = this.configService.get('auth.refresh_token_ttl'); // 30 * 24 * 60 * 60 * 1000
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: nodeEnv === "production",
+      sameSite: "lax" as const,
+      path: "/",
+      domain: nodeEnv === "production" ? `.${domain}` : undefined,
+    };
+
+    response.cookie("accessToken", tokens.accessToken, {
+      ...cookieOptions,
+      maxAge: accessTokenTTL,
+    });
+
+    response.cookie("refreshToken", tokens.refreshToken, {
+      ...cookieOptions,
+      path: "/auth/refresh", //TODO: May cause errors, should be reviewed whether it works correctly
+      maxAge: refreshTokenTTL,
+    });
+
+    return response;
+  }
+}
