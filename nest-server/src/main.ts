940a7129aed420b55e5dba0e2ab4047cd3cd622c
Cenaure <discordjahysama@gmail.com>
2026-07-28 22:28:55 +0200

    fix(docker): fix port mapping

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import {ConfigService} from "@nestjs/config";
 import {ValidationPipe} from "@nestjs/common";
 
@@ -13,6 +13,6 @@ async function bootstrap() {
     transform: true
   }));
 
-  await app.listen(serverPort!);
+  await app.listen(serverPort!, "0.0.0.0");
 }
 bootstrap();
