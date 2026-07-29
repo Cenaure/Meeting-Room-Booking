d56da8b022f72abc2e5f349e3ea607994630109e
Cenaure <discordjahysama@gmail.com>
2026-07-29 23:14:29 +0200

    feat(config): add office timezone to app config

--- nest-server/src/configurations/app.config.ts
+++ nest-server/src/configurations/app.config.ts
@@ -8,11 +8,13 @@ export default registerAs("app", () => {
     {
       node_env: process.env.NODE_ENV || "development",
       server_port: parseInt(process.env.SERVER_PORT!, 10) || 3000,
+      office_timezone: process.env.OFFICE_TIMEZONE!,
     },
 
     Joi.object({
       node_env: Joi.string().valid("development", "production", "test").required(),
       server_port: Joi.number().required(),
+      office_timezone: Joi.string().required(),
     })
   )
 })
