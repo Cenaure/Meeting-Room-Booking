d56ca61ddc6a734df9156b6be557804432a4e881
Cenaure <discordjahysama@gmail.com>
2026-07-29 15:25:50 +0200

    feat(config): add google client envs to auth config

--- nest-server/src/configurations/auth.config.ts
+++ nest-server/src/configurations/auth.config.ts
@@ -10,6 +10,9 @@ export default registerAs("auth", () => {
       activation_link_ttl: parseInt(process.env.ACTIVATION_LINK_TTL!, 10),
       access_token_secret: process.env.ACCESS_TOKEN_SECRET!,
       refresh_token_secret: process.env.REFRESH_TOKEN_SECRET!,
+      google_client_id: process.env.GOOGLE_CLIENT_ID!,
+      google_client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+      google_callback_url: process.env.GOOGLE_CALLBACK_URL!,
     },
 
     Joi.object({
@@ -18,6 +21,9 @@ export default registerAs("auth", () => {
       activation_link_ttl: Joi.number().required(),
       access_token_secret: Joi.string().required(),
       refresh_token_secret: Joi.string().required(),
+      google_client_id: Joi.string().required(),
+      google_client_secret: Joi.string().required(),
+      google_callback_url: Joi.string().required(),
     })
   )
 })
