04fda158241823a62f46e52bc6d77f608b0c4bf3
Cenaure <discordjahysama@gmail.com>
2026-07-30 18:18:17 +0200

    test(reservations): implement unit tests for getReservations method

--- nest-server/src/modules/reservations/reservations.service.spec.ts
+++ nest-server/src/modules/reservations/reservations.service.spec.ts
@@ -35,6 +35,12 @@ describe('ReservationsService', () => {
   const mockJob = {
     waitUntilFinished: jest.fn()
   };
+
+  const databaseService = {
+    reservation: {
+      findMany: jest.fn()
+    }
+  }
   //endregion: # mocks
 
   beforeEach(async () => {
@@ -42,7 +48,7 @@ describe('ReservationsService', () => {
       providers: [ReservationsService,
         {
           provide: DatabaseService,
-          useValue: {}
+          useValue: databaseService
         },
         {
           provide: ConfigService,
@@ -248,5 +254,101 @@ describe('ReservationsService', () => {
           })
       });
     })
+  })
+
+  describe("get reservations", () => {
+    // These tests fulfill the server technical requirements
+    // "Юніт-тести на логіку перетину інтервалів: впритул, часткове перекриття, повний збіг, сусідні дні"
+
+    const date = DateTime.now().plus({day: 1}).set({minute: 0, second: 0, millisecond: 0}).setZone("Europe/Kyiv");
+
+    beforeEach(() => {
+      const reservationsInTheDb = [
+        {
+          room_id: 1,
+          time_start: date.set({hour: 9}).toJSDate(),
+          time_end: date.set({hour: 10}).toJSDate()
+        },
+        {
+          room_id: 1,
+          time_start: date.set({hour: 11, minute: 30}).toJSDate(),
+          time_end: date.set({hour: 12, minute: 30}).toJSDate()
+        },
+        {
+          room_id: 2,
+          time_start: date.set({hour: 10}).toJSDate(),
+          time_end: date.set({hour: 11, minute: 30}).toJSDate()
+        }
+      ]
+
+      // Prisma findMany implementation
+      databaseService.reservation.findMany.mockImplementation(async (props) => {
+        const {room_id, time_start, time_end} = props.where;
+
+        const requestedEnd = time_start.lt;
+        const requestedStart = time_end.gt;
+
+        return reservationsInTheDb.filter((reservation) => {
+          const isSameRoom = reservation.room_id === room_id;
+
+          const isOverlapping =
+            reservation.time_start < requestedEnd &&
+            reservation.time_end > requestedStart;
+
+          return isSameRoom && isOverlapping;
+        });
+      });
+    })
+
+    // Touching other reservations' intervals
+    it('shouldn\'t return any reservations if reservation we\'re searching for is touching some interval from the right', async () => {
+      const ourStart = date.set({hour: 10}).toJSDate()
+      const ourEnd = date.set({hour: 11}).toJSDate()
+
+      await expect(service.getReservations(1, ourStart, ourEnd)).resolves.toHaveLength(0);
+    });
+
+    it('shouldn\'t return any reservations if reservation we\'re searching for is touching some interval from the left', async () => {
+      const ourStart = date.set({hour: 9, minute: 30}).toJSDate()
+      const ourEnd = date.set({hour: 10}).toJSDate()
+
+      await expect(service.getReservations(2, ourStart, ourEnd)).resolves.toHaveLength(0);
+    });
+
+    it('shouldn\'t return any reservations if reservation we\'re searching for is touching some intervals from both sides', async () => {
+      const ourStart = date.set({hour: 10}).toJSDate()
+      const ourEnd = date.set({hour: 11, minute: 30}).toJSDate()
+
+      await expect(service.getReservations(1, ourStart, ourEnd)).resolves.toHaveLength(0);
+    });
+
+    // Crossing reservations
+    it('should return a reservation if we\'re crossing its interval from the right', async () => {
+      const ourStart = date.set({hour: 12}).toJSDate()
+      const ourEnd = date.set({hour: 13, minute: 30}).toJSDate()
+
+      await expect(service.getReservations(1, ourStart, ourEnd)).resolves.not.toHaveLength(0);
+    });
+
+    it('should return a reservation if we\'re crossing its interval from the left', async () => {
+      const ourStart = date.set({hour: 11}).toJSDate()
+      const ourEnd = date.set({hour: 12}).toJSDate()
+
+      await expect(service.getReservations(1, ourStart, ourEnd)).resolves.not.toHaveLength(0);
+    });
+
+    it('should return a reservation if we\'re crossing its interval completely', async () => {
+      const ourStart = date.set({hour: 9}).toJSDate()
+      const ourEnd = date.set({hour: 10}).toJSDate()
+
+      await expect(service.getReservations(1, ourStart, ourEnd)).resolves.not.toHaveLength(0);
+    });
+
+    it('shouldn\t return a reservation if we\'re crossing its interval completely on the next day', async () => {
+      const ourStart = date.plus({day: 1}).set({hour: 9}).toJSDate()
+      const ourEnd = date.plus({day: 1}).set({hour: 10}).toJSDate()
+
+      await expect(service.getReservations(1, ourStart, ourEnd)).resolves.toHaveLength(0);
+    });
   })
 });
