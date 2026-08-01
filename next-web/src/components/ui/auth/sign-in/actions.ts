4a43e51fec9d446aa052ec1804db83c4eda8370c
Cenaure <discordjahysama@gmail.com>
2026-08-02 00:41:03 +0200

    feat(web/auth): finalize sign-in form with actions

---
+++ next-web/src/components/ui/auth/sign-in/actions.ts
@@ -0,0 +1,22 @@
+"use server";
+import setTokenCookies from "@/lib/auth/set-token-cookies";
+import {User} from "@/models/user";
+import createInstance from "@/utils/http";
+import {failure, parseApiError, success} from "@/lib/errors/api-errors-handler";
+
+export async function signIn(
+  email: string,
+  password: string
+) {
+  try {
+    const instance = createInstance();
+    const response = await instance.post(`/auth/sign-in`, {email, password});
+
+    if (response.data.accessToken) await setTokenCookies(response.data);
+
+    return success<{ accessToken: string; refreshToken: string, user: User }>(response.data);
+  } catch (err: any) {
+    const message = parseApiError(err)
+    return failure(message);
+  }
+}
