3ba7e82cca691e1629899e908c3ea7369530dbaf
Cenaure <discordjahysama@gmail.com>
2026-08-02 00:27:38 +0200

    feat(web/ui): add button component using CVA

---
+++ next-web/src/components/ui/shared/button/button.tsx
@@ -0,0 +1,71 @@
+import {cva, type VariantProps} from "class-variance-authority";
+import {ButtonHTMLAttributes, ReactNode} from "react";
+
+const buttonStyles = cva(
+  [
+    "inline-flex items-center justify-center gap-2",
+    "rounded-md font-medium tracking-tight",
+    "transition-all duration-100 ease-out",
+    "disabled:opacity-40 disabled:cursor-not-allowed disabled:pointer-events-none",
+    "active:scale-[0.98]",
+  ],
+  {
+    variants: {
+      variant: {
+        primary: [
+          "bg-lavender-950 text-lavender-50 dark:bg-zinc-800/80",
+          "ring-1 ring-border",
+          "hover:bg-lavender-900 hover:ring-gray-700  dark:hover:bg-zinc-700/50 dark:hover:ring-zinc-800 hover:text-white",
+        ],
+        outline: [
+          "bg-surface-3/25",
+          "ring-1 ring-border",
+          "hover:bg-surface-3/50 hover:ring-gray-300"
+        ],
+        ghost: "bg-transparent text-foreground hover:bg-zinc-200/90 dark:hover:bg-zinc-800/90",
+      },
+      size: {
+        auto: "text-sm",
+        sm: "h-6 px-3 text-sm",
+        md: "h-8 px-4 text-sm",
+        lg: "h-9 px-6 text-base",
+      },
+      fullWidth: {
+        true: "w-full"
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonStyles> {
+  children: ReactNode;
+  loading?: boolean;
+}
+
+export default function Button({
+                                 children,
+                                 variant,
+                                 size,
+                                 loading,
+                                 disabled,
+                                 fullWidth,
+                                 className,
+                                 ...props
+                               }: ButtonProps) {
+  return (
+    <button
+      className={buttonStyles({variant, size, fullWidth, className})}
+      disabled={disabled || loading}
+      {...props}
+    >
+      {loading && null}
+      {children}
+    </button>
+  );
+}
