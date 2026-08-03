2e4e2615a773d5f11edd6a68fd61f4ec0a845597
Cenaure <discordjahysama@gmail.com>
2026-08-03 13:21:35 +0200

    chore(web): add missing routes

--- next-web/src/lib/routes.ts
+++ next-web/src/lib/routes.ts
@@ -1,3 +1,7 @@
 export const auth_route = '/auth'
 export const sign_in_route = auth_route + '/sign-in'
 export const sign_up_route = auth_route + '/sign-up'
+
+export const settings_route = '/settings'
+export const profile_route = settings_route + '/profile'
+export const appearance_route = settings_route + '/appearance'
