24aa978a8ee7894a3c386b141e6403c05c5e7c11
Cenaure <discordjahysama@gmail.com>
2026-08-05 00:42:02 +0200

    chore(server): update basic rooms names

--- nest-server/src/modules/rooms/rooms.service.ts
+++ nest-server/src/modules/rooms/rooms.service.ts
@@ -1,42 +1,38 @@
-import {Injectable, OnModuleInit} from '@nestjs/common';
-import {DatabaseService} from "../../database/database.service";
-import GetRoomsDto from "./dto/get-rooms.dto";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { DatabaseService } from '../../database/database.service';
+import GetRoomsDto from './dto/get-rooms.dto';
 
 @Injectable()
 export class RoomsService implements OnModuleInit {
-  constructor(
-    private readonly databaseService: DatabaseService,
-  ) {
-  }
+  constructor(private readonly databaseService: DatabaseService) {}
 
   /**
    * Insert rooms to the database on module initialization
    */
   async onModuleInit() {
     const rooms = [
-      {title: "Кімната #001", floor: 1, capacity: 10},
-      {title: "Кімната #003", floor: 1, capacity: 4},
-      {title: "Кімната #010", floor: 1, capacity: 15},
-      {title: "Кімната #102", floor: 2, capacity: 7},
-      {title: "Кімната #104", floor: 2, capacity: 5},
-      {title: "Кімната #201", floor: 3, capacity: 14},
-    ]
+      { title: 'Акварель', floor: 1, capacity: 10 },
+      { title: 'Атлас', floor: 1, capacity: 4 },
+      { title: 'Венера', floor: 1, capacity: 15 },
+      { title: 'Дюна', floor: 2, capacity: 7 },
+      { title: 'Каїн', floor: 2, capacity: 5 },
+      { title: 'Акемі', floor: 3, capacity: 14 },
+    ];
 
     for (const room of rooms) {
       await this.databaseService.room.upsert({
-        where: {title: room.title},
-        update: {},
+        where: { title: room.title },
+        update: { title: room.title },
         create: room,
       });
     }
   }
 
-
   async getRooms(query: GetRoomsDto) {
     const rooms = await this.databaseService.room.findMany({
-      where: {capacity: {gte: query.wishedCapacity}},
+      where: { capacity: { gte: query.wishedCapacity } },
       skip: (query.page - 1) * query.limit,
-      take: query.limit
+      take: query.limit,
     });
 
     return {
@@ -46,6 +42,6 @@ export class RoomsService implements OnModuleInit {
   }
 
   async findById(roomId: number) {
-    return this.databaseService.room.findFirst({where: {id: roomId}});
+    return this.databaseService.room.findFirst({ where: { id: roomId } });
   }
 }
