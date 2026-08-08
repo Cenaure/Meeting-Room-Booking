4a9a3b3a5d8d74792629e36f6d8782df7592a68a
Cenaure <discordjahysama@gmail.com>
2026-08-08 18:35:19 +0200

    fix(server/notification): fix notification handler setting incorrect time

--- nest-server/src/modules/notifications/handlers/reservation-ending-soon.handler.ts
+++ nest-server/src/modules/notifications/handlers/reservation-ending-soon.handler.ts
@@ -1,38 +1,48 @@
-import {INotificationJobData, NotificationHandler} from "./notification-handler.interface";
-import {Injectable} from "@nestjs/common";
-import {ReservationsService} from "../../reservations/reservations.service";
-import {Job} from "bullmq";
-import {NotificationsService} from "../services/notifications.service";
-import {NotificationsGateway} from "../notifications.gateway";
-import {NotificationType} from "../utils/notification-types.constants";
+import {
+  INotificationJobData,
+  NotificationHandler,
+} from './notification-handler.interface';
+import { Injectable } from '@nestjs/common';
+import { ReservationsService } from '../../reservations/reservations.service';
+import { Job } from 'bullmq';
+import { NotificationsService } from '../services/notifications.service';
+import { NotificationsGateway } from '../notifications.gateway';
+import { NotificationType } from '../utils/notification-types.constants';
 
 export interface IReservationEndingSoonJobData extends INotificationJobData {
   reservationId: string;
   nextReservationId: string;
+  body: { free_room_before: Date };
 }
 
 @Injectable()
 class ReservationEndingSoonHandler implements NotificationHandler<IReservationEndingSoonJobData> {
-  readonly type: typeof NotificationType.ReservationEndingSoon;
+  readonly type = NotificationType.ReservationEndingSoon;
 
   constructor(
     private readonly reservationsService: ReservationsService,
     private readonly notificationsService: NotificationsService,
-    private readonly notificationGateway: NotificationsGateway
-  ) {
-  }
+    private readonly notificationGateway: NotificationsGateway,
+  ) {}
 
-  async processNotification(job: Job<IReservationEndingSoonJobData>): Promise<void> {
-    const {reservationId, nextReservationId, body, userId} = job.data
+  async processNotification(
+    job: Job<IReservationEndingSoonJobData>,
+  ): Promise<void> {
+    const { reservationId, nextReservationId, body, userId } = job.data;
 
     const [currentReservation, nextReservation] = await Promise.all([
       this.reservationsService.findReservationById(reservationId),
       this.reservationsService.findReservationById(nextReservationId),
-    ])
+    ]);
 
     // Checks whether the reservations exist and are active (not canceled)
-    if (!currentReservation || !nextReservation || currentReservation.status != "active" || nextReservation.status != "active")
-      return
+    if (
+      !currentReservation ||
+      !nextReservation ||
+      currentReservation.status != 'active' ||
+      nextReservation.status != 'active'
+    )
+      return;
 
     try {
       await this.notificationsService.createNotification({
@@ -40,17 +50,20 @@ class ReservationEndingSoonHandler implements NotificationHandler<IReservationEn
         reservation_id: reservationId,
         type: this.type,
         body: {
-          free_before_date: nextReservation.time_start
+          free_before_date: body.free_room_before,
         },
-        sent_at: new Date()
-      })
-    } catch {
+        sent_at: new Date(),
+      });
+    } catch (err) {
       // Notification duplicate
-      return
+      return;
     }
 
-    this.notificationGateway.sendReservationEndingReminder(userId, nextReservation.time_start)
+    this.notificationGateway.sendReservationEndingReminder(
+      userId,
+      nextReservation.time_start,
+    );
   }
 }
 
-export default ReservationEndingSoonHandler
+export default ReservationEndingSoonHandler;
--- nest-server/src/modules/notifications/services/notifications.service.ts
+++ nest-server/src/modules/notifications/services/notifications.service.ts
@@ -1,70 +1,76 @@
-import {Injectable} from '@nestjs/common';
-import CreateNotificationDto from "../dto/create-notification.dto";
-import {DatabaseService} from "../../../database/database.service";
-import {UsersService} from "../../users/users.service";
-import {AppException} from "../../../common/errors/app-exception";
-import {AppExceptionBodyCode} from "../../../common/errors/app-exception-body.interface";
+import { Injectable } from '@nestjs/common';
+import CreateNotificationDto from '../dto/create-notification.dto';
+import { DatabaseService } from '../../../database/database.service';
+import { UsersService } from '../../users/users.service';
+import { AppException } from '../../../common/errors/app-exception';
+import { AppExceptionBodyCode } from '../../../common/errors/app-exception-body.interface';
 
 @Injectable()
 export class NotificationsService {
   constructor(
     private readonly databaseService: DatabaseService,
     private readonly usersService: UsersService,
-  ) {
-  }
+  ) {}
 
   async createNotification(dto: CreateNotificationDto) {
     const user = await this.usersService.findById(dto.user_id);
     if (!user)
-      throw AppException.notFound({code: AppExceptionBodyCode.userNotFound, message: "User not found"})
+      throw AppException.notFound({
+        code: AppExceptionBodyCode.userNotFound,
+        message: 'User not found',
+      });
 
     if (dto.reservation_id) {
       const reservation = await this.databaseService.reservation.findFirst({
-        where: {id: dto.reservation_id}
-      })
+        where: { id: dto.reservation_id },
+      });
       if (!reservation)
-        throw AppException.notFound({code: AppExceptionBodyCode.reservationNotFound, message: "Reservation not found"})
+        throw AppException.notFound({
+          code: AppExceptionBodyCode.reservationNotFound,
+          message: 'Reservation not found',
+        });
     }
 
-    this.databaseService.notification.create({
+    return this.databaseService.notification.create({
       data: dto,
-    })
+    });
   }
 
   async markNotificationAsRead(userId: number, notificationId: string) {
-    const notification = await this.findNotificationById(notificationId)
+    const notification = await this.findNotificationById(notificationId);
     // Error codes have no actual sense as user can't see not existing notification or another user's notification
     // It's just a precaution if someone calls API directly
-    if (!notification)
-      throw AppException.notFound()
+    if (!notification) throw AppException.notFound();
 
-    if (notification.user_id !== userId)
-      throw AppException.forbidden()
+    if (notification.user_id !== userId) throw AppException.forbidden();
 
-    if (notification.read_at)
-      return {success: true}
+    if (notification.read_at) return { success: true };
 
     await this.databaseService.notification.update({
-      where: {id: notificationId},
-      data: {read_at: new Date()}
-    })
+      where: { id: notificationId },
+      data: { read_at: new Date() },
+    });
 
-    return {success: true}
+    return { success: true };
   }
 
   async markAllNotificationsAsRead(userId: number) {
     const result = await this.databaseService.notification.updateMany({
-      where: {user_id: userId, read_at: null},
-      data: {read_at: new Date()},
+      where: { user_id: userId, read_at: null },
+      data: { read_at: new Date() },
     });
-    return {success: true, updated: result.count};
+    return { success: true, updated: result.count };
   }
 
   async findNotificationById(notificationId: string) {
-    return this.databaseService.notification.findFirst({where: {id: notificationId}})
+    return this.databaseService.notification.findFirst({
+      where: { id: notificationId },
+    });
   }
 
   async findMyUnreadNotifications(userId: number) {
-    return this.databaseService.notification.findMany({where: {user_id: userId, read_at: null}})
+    return this.databaseService.notification.findMany({
+      where: { user_id: userId, read_at: null },
+    });
   }
 }
